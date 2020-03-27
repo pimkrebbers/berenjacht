aeee5940b7f641f85bae8a5e3dc8198403b3fb1f
Pim Krebbers <pimkrebbers@gmail.com>
2020-03-27 14:09:32 +0100

    Test version

---
+++ map2.js
@@ -0,0 +1,77 @@
+var map;
+var locations = [];
+
+function initialiseMap() {
+
+  // Load data from an example Google spreadsheet that contains latitude and longitude columns using Google Sheets API v4 that returns JSON.
+  // Replace the ID of your Google spreadsheet and you API key in the URL:
+  // https://sheets.googleapis.com/v4/spreadsheets/ID_OF_YOUR_GOOGLE_SPREADSHEET/values/Sheet1!A2:Q?key=YOUR_API_KEY
+  // Also make sure your API key is authorised to access Google Sheets API - you can enable that through your Google Developer console.
+  // Finally, in the URL, fix the sheet name and the range that you are accessing from your spreadsheet. 'Sheet1' is the default name for the first sheet.
+  $.getJSON("https://sheets.googleapis.com/v4/spreadsheets/1B0TkgNxiD0Mn4KpEK3VB8rGuizEBlR_x0tKnfhxS8yY/values/Beren!A2:Q?key=AIzaSyDapnAfTmAxWjt56_vWZM9lLobV-V6y9HE", function(data) {
+    	// data.values contains the array of rows from the spreadsheet. Each row is also an array of cell values.
+    	// Modify the code below to suit the structure of your spreadsheet.
+    	$(data.values).each(function() {
+    		var location = {};
+				location.title = this[0];
+        
+        if (this[2]) {
+        
+          location.latitude = parseFloat(this[2]);
+          location.longitude = parseFloat(this[3]);
+          //location.institution = this[3];
+          //location.department = this[4];
+          //location.funder = this[0];
+          //location.url = this[13];
+          locations.push(location);
+        }
+    	});
+
+      // Center on (0, 0). Map center and zoom will reconfigure later (fitbounds method)
+      var mapOptions = {
+        zoom: 10,
+        center: new google.maps.LatLng(0, 0)
+      };
+      map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      setLocations(map, locations);
+      
+      google.maps.event.addListener(map, "bounds_changed", function() {
+         // send the new bounds back to your server
+         alert("map bounds{"+map.getBounds());
+      });
+  });
+}
+
+
+function setLocations(map, locations) {
+  var bounds = new google.maps.LatLngBounds();
+  // Create nice, customised pop-up boxes, to appear when the marker is clicked on
+  var infowindow = new google.maps.InfoWindow({
+    content: "Content String"
+  });
+  for (var i = 0; i < locations.length; i++) {
+    var new_marker = createMarker(map, locations[i], infowindow);
+    bounds.extend(new_marker.position);
+  }
+  map.fitBounds(bounds);
+}
+
+function createMarker(map, location, infowindow) {
+
+  // Modify the code below to suit the structure of your spreadsheet (stored in variable 'location')
+  var position = {
+    lat: parseFloat(location.latitude),
+    lng: parseFloat(location.longitude)
+  };
+  var marker = new google.maps.Marker({
+    position: position,
+    map: map,
+    title: location.title,
+    icon: 'https://pimkrebbers.github.io/berenjacht/Bear.png'
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent('<div style="color: black;"><p><strong>' + this.title + '</strong></p></div>');
+    infowindow.open(map, marker);
+  });
+  return marker;
+}
